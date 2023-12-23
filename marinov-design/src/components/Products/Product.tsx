523b7cee6eb5a25f4490a36870c8795cc8697b2d
Konstantin <kvckov@yahoo.com>
2023-12-23 19:33:03 +0100

    product component created,types created,just need to be implemented

--- marinov-design/src/components/Products/Product.tsx
+++ marinov-design/src/components/Products/Product.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import style from "./style.module.css";
+import { IProductProps } from "@/types/ProjectTypes";
 
-const Product = () => {
+const Product = ({ images, price, name, click }: IProductProps) => {
   return (
-    <div><h1>Product</h1></div>
-  )
-}
+    <div onClick={click} className={style.Product}>
+      <img src={images[0]} alt="product-img" />
+      <p>{name}</p>
+      <p>{price}</p>
+    </div>
+  );
+};
 
-export default Product
+export default Product;
